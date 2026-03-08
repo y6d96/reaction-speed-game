7ac8e5830a8c6822b783a57601477b3c06d14599
Wisdom Uzo <y6d96@students.keele.ac.uk>
2026-03-08 19:37:52 +0100

    Display reaction time in milliseconds

--- reaction_game.py
+++ reaction_game.py
@@ -11,11 +11,16 @@ window.geometry("400x300")
 # Game title label
 label = tk.Label(window, text="Reaction Speed Game", font=("Arial", 20))
 label.pack(pady=40)
+label.config(bg="green")
 
 start_time = 0
 
 def start_game():
     window.after(random.randint(2000, 5000), show_green)
+
+def click_screen(event):
+    reaction = time.time() - start_time
+    label.config(text=f"Reaction time: {round(reaction*1000)} ms", bg="white")
 
 def show_green():
     global start_time
